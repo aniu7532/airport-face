3a6573e9a76c6ff0dd4e88f46e3ee5fbbd3b9c0b
zhang liang <>
2026-02-28 16:41:18 +0800

    解决glide下载图片失败的问题

--- app/src/main/java/com/arcsoft/arcfacedemo/util/glide/SecureGlideModule.java
+++ app/src/main/java/com/arcsoft/arcfacedemo/util/glide/SecureGlideModule.java
@@ -1,33 +1,34 @@
 package com.arcsoft.arcfacedemo.util.glide;
 
 import java.io.File;
+import java.io.InputStream;
 
 import javax.crypto.SecretKey;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.load.model.ModelLoaderFactory;
 import com.bumptech.glide.module.AppGlideModule;
+import com.arcsoft.arcfacedemo.network.OkHttpUtils;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+
+import okhttp3.OkHttpClient;
 
 @GlideModule
 public class SecureGlideModule extends AppGlideModule {
     @Override
     public void registerComponents(Context context, Glide glide, Registry registry) {
+        // 使用信任所有证书的 OkHttpClient，解决 HTTPS 自签名/私有证书导致的 SSLHandshakeException（OkHttpClient 实现了 Call.Factory，可传入 Factory）
+        OkHttpClient unsafeClient = OkHttpUtils.getUnsafeOkHttpClient();
+        ModelLoaderFactory<GlideUrl, InputStream> urlLoaderFactory = new OkHttpUrlLoader.Factory(unsafeClient);
+        registry.replace(GlideUrl.class, InputStream.class, urlLoaderFactory);
         // 注册加密文件解码器
         SecretKey secretKey = AESUtils.generateKey();
         registry.append(File.class, Bitmap.class, new EncryptedFileDecoder(glide.getBitmapPool(), secretKey));
-
-        // // 使用自定义的 OkHttpClient
-        // OkHttpClient okHttpClient = NetworkConfig.createOkHttpClient();
-        // registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(okHttpClient));
-
-        // OkHttpClient client = new OkHttpClient.Builder().connectTimeout(15, TimeUnit.SECONDS) // 设置连接超时时间
-        // .readTimeout(30, TimeUnit.SECONDS) // 设置读取超时时间
-        // .build();
-        //
-        // registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(client));
     }
 }
