49293bb7805a39f680de94839a03c09a7a30f007
zhang liang <>
2026-06-16 23:38:44 +0800

    打包

--- app/src/yinchuan/java/com/arcsoft/arcfacedemo/config/ChannelConfig.java
+++ app/src/yinchuan/java/com/arcsoft/arcfacedemo/config/ChannelConfig.java
@@ -5,9 +5,9 @@ package com.arcsoft.arcfacedemo.config;
  */
 public final class ChannelConfig {
 
-    public static final String BASE_URL_Test0 = "https://inc-kzqtxz.caacsri.com";// 二所测试环境
+//    public static final String BASE_URL = "https://inc-kzqtxz.caacsri.com";// 二所测试环境
     public static final String BASE_URL = "https://inckzqtxz.caacsri.com";// 二所正式环境
-    public static final String BASE_URL_Test1 = "http://test.sczhbf.com:58088";//慧能测试环境
+//    public static final String BASE_URL = "http://test.sczhbf.com:58088";//慧能测试环境
     /** 无租户路径前缀，接口为 域名/app-api/... */
     public static final String TENANT_PREFIX = "";
     public static final String TENANT_ID = "1";
